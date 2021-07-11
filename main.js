2463bfb1019b003e18b9c19804c8f54f59b751f5
Borislav Borislavov <borislavkb@gmail.com>
2021-07-11 10:30:40 +0200

    Add filter to influence cards rendered

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 import { renderCard } from "./lib/card.js";
-// import { fetchCharacters } from "./lib/db";
+import { fetchCharacters } from "./lib/db.js";
 
 const formButton = document.querySelector(".form");
-const filter = document.querySelector("select");
+export const filter = document.querySelector("select");
 const main = document.querySelector("main");
 
 formButton.addEventListener("submit", (event) => {
-  const url = "https://rickandmortyapi.com/api/character";
-  fetch(url)
-    .then((response) => {
-      console.log(response.status);
-      return response.json();
-    })
-    .then((data) => {
-      data.results.forEach((character) => {
-        main.append(renderCard(character));
-      });
+  const status = filter.value;
+  clearAll();
+  fetchCharacters(status).then((data) => {
+    data.results.forEach((character) => {
+      main.append(renderCard(character));
     });
+  });
 
   event.preventDefault();
 });
+
+function clearAll() {
+  const characterList = document.querySelectorAll(".card");
+  characterList.forEach((character) => character.remove());
+}
 
 // const status = filter.value;
 // fetchCharacters(status).then((status) => {
@@ -28,3 +29,23 @@ formButton.addEventListener("submit", (event) => {
 //     main.append(character);
 //   });
 // });
+
+//Working fetch
+// const url = "https://rickandmortyapi.com/api/character";
+//   fetch(url)
+//     .then((response) => {
+//       console.log(response.status);
+//       return response.json();
+//     })
+//     .then((data) => {
+//       data.results.forEach((character) => {
+//         main.append(renderCard(character));
+//       });
+//     });
+
+//   event.preventDefault();
+
+// fetch(url)
+//     .then((response) => {
+//       return response.json();
+//     })
